60efe84f04806cd448219b6fd2ad629ba9a3c489
zeina101 <zeina.syedain@cas.edu.pk>
2025-04-17 20:34:22 +0500

    Add files via upload

---
+++ student_detail.py
@@ -0,0 +1,22 @@
+class Person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+
+        def printname(self):
+            print(self.firstname.lastname)
+
+    class Student(def __init___(self, fname, lname, yeare):
+        super().__init__(fname, lname)
+        self.graduationyeaer = year
+
+        x = Student("Joey", "King", 2021)
+
+        x.printname()
+        print(x.graduationyear)
+
+        y = Student("Rahul", "Kumar", 2022)
+        y.printname()
+        print(y.graduationyear)
+
+
